8f215270fe3ec4952d3f6957b8304ee32dc76637
Jose Rodriguez Guillen <jose.rodriguez@avantica.net>
2019-04-28 23:36:49 -0500

    improve algorithm when user receives messages

--- chat.js
+++ chat.js
@@ -1,13 +1,15 @@
 let appId = 76776;
 let diaglogsList = []
-let newMessages = []
+let diaglogHistory = []
 let userList = []
 let userId = false;
+let currentOpponentId = false;
 
 var init = function (){
     quickbloxInitConfig()
     listenBtns()
     createSession()
+    forceSignUp()
 }
 
 let forceSignUp = function () {
@@ -82,9 +84,9 @@ let connectChat = function (userId, userPass) {
             console.log('error connecting chat, user => ',user,   err);
         } else {
             console.log('chat connected', roster)
-            listAllUsers(userId)
+            $('.js-spinner-users').addClass('d-none')
             getDialogs()
-            QB.chat.onMessageListener = listtenMessages;
+            QB.chat.onMessageListener = listenMessages;
          /*
          *  (Object) roster - The user contact list
          *  roster = {
@@ -99,6 +101,8 @@ let connectChat = function (userId, userPass) {
 }
 
 let login = function() {
+    $('.js-txt-login').addClass('d-none')
+    $('.js-spinner-login-btn').removeClass('d-none')
     var params = {
         login: $('#txtEmailSignIn').val(),
         password: $('#txtPassSignIn').val(),
@@ -112,6 +116,10 @@ let login = function() {
     
     QB.login(params, function(err, result) {
         if(result){
+            $('.js-txt-login').removeClass('d-none')
+            $('.js-spinner-login-btn').addClass('d-none')
+            $('.js-spinner-users').removeClass('d-none')
+            $('.js-loading-users').hide();
             connectChat(result.id, params.password)
             $('#modal-signIn').modal('hide')
             userId = result.id;
@@ -162,85 +170,123 @@ let listAllUsers = function(ignoreId) {
         console.log('list of users', users)
         userList = users;
         renderUsersList(users, ignoreId)
-        $('.js-user').on('click', function (){
-            let opponentId = $(this).data('id')
-            let currentOponentId =  $('.messages-container').data('opponentId')
-            if(currentOponentId != opponentId) {
-                $('.messages-container').data('opponentId', opponentId)
-                handleDialog(opponentId)
-            }
-            console.log('user selected => ', opponentId)
-        })
-
+        listenSelectUserEvent()
     } else {
         console.log('something went wrong retrieving the list of users', users)
     }
     });
 }
 
+let listenSelectUserEvent = function (){
+    $('.js-user').on('click', function (){
+        $('.js-user').removeClass('user-selected')
+        $(this).addClass('user-selected')
+        let opponentId = $(this).data('id')
+        if(currentOpponentId != opponentId) {
+            currentOpponentId = opponentId
+            handleDialog(opponentId)
+        }
+        console.log('user selected => ', opponentId)
+    })
+}
+
 let handleDialog = function (opponentId) {
+    cleanChat()
     let dialogId = null;
-    diaglogsList.forEach(function (dialog) {
+    for (let i = 0; i < diaglogsList.length; i++) {
+        let dialog = diaglogsList[i];
         if(dialog.type == 3 && dialog.occupants_ids.indexOf(opponentId) != -1) {
             dialogId = dialog._id;
         }
-    })
-    console.log('handle dialog', dialogId)
-    if(dialogId != null) {
+    }
+    console.log('handle dialog', dialogId, diaglogHistory)
+    if(!diaglogHistory[dialogId] && dialogId) {
         getDialog(dialogId)
-    } else {
-        createDialog(oponnetId)
+    }else if(dialogId){
+        getDialogFromMemory(dialogId)
+    }
+    updateUserBadge(opponentId, true)
+}
+
+let scrollTobottom = function (smooth = false) {
+    console.log('scrolling')
+    $('.messages-container').animate({
+        scrollTop: $('.messages-container .messages-list').height()
+    }, smooth ? 400 : 0);
+}
+
+let cleanChat = function (showNoMessage = false) {
+    $('.messages-container .messages-list').html('')
+    if(showNoMessage) {
+        $('.js-no-messages').show()
     }
 }
 
-let getDialog = function (dialogId) {
+let getDialogFromMemory = async function (dialogId) {
+    cleanChat()
+    await diaglogHistory[dialogId].items.reverse().forEach(function (element) {
+        let type = element.sender_id == userId ? 'send' : 'received';
+        renderMessage(type , element.message)
+    })
+    scrollTobottom()
+}
+
+let getDialog = async function (dialogId) {
     var params = {chat_dialog_id: dialogId, sort_desc: 'date_sent', limit: 100, skip: 0};
-    QB.chat.message.list(params, function(err, messages) {
-    if (messages) {
-        messages.items.reverse().forEach(function (element) {
-            let type = element.sender_id == userId ? 'send' : 'received';
-            renderMessage(type , element.message)
-        })
-        console.log('chat history', messages)
-    }else{
-        console.log(err);
-    }
+    await QB.chat.message.list(params, function(err, messages) {
+        if (messages) {
+            cleanChat()
+            diaglogHistory[dialogId] = messages;
+            let arrayItems = messages.items.reverse();
+            for (let i = 0; i < arrayItems.length; i++) {
+                let element = arrayItems[i];
+                let type = element.sender_id == userId ? 'send' : 'received';
+                renderMessage(type , element.message)
+            }
+            scrollTobottom()
+            console.log('chat history', messages)
+        }else{
+            console.log(err);
+        }
     });
 }
 
-let createDialog = function (oponnetId) {
+let  createDialog = async function (oponnetId) {
     var params = {
         type: 3,
         occupants_ids: [oponnetId]
       };
        
-      QB.chat.dialog.create(params, function(err, createdDialog) {
+      await QB.chat.dialog.create(params, function(err, createdDialog) {
         if (err) {
           console.log(err);
         } else {
             console.log('dialog created', createdDialog);
             getDialogs()
+            return createdDialog
         }
       });
+      return 0;
 }
 
-let getDialogs = function () {
+let getDialogs = async function () {
     var filters = null;
-    QB.chat.dialog.list(filters, function(err, resDialogs) {
-    if (err) {
-        console.log(err);
-    } else {
-        diaglogsList = resDialogs.items;
-        console.log(resDialogs);
-    }
+    await QB.chat.dialog.list(filters, function(err, resDialogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            diaglogsList = resDialogs.items;
+            listAllUsers(userId)
+            console.log('list of dialogs', resDialogs);
+            return;
+        }
     });
 }
 
 let sendMessage = function () {
     let message = $('.js-message').val();
-    let opponentId = $('.messages-container').data('opponentId');
-    console.log('send to user id => ', opponentId, 'message => ', message)
-    if(message.length == 0 || !opponentId) {
+    console.log('send to user id => ', currentOpponentId, 'message => ', message)
+    if(message.length == 0 || !currentOpponentId) {
         return;
     }
     var msg = {
@@ -250,9 +296,27 @@ let sendMessage = function () {
           save_to_history: 1,
         }
       };
-      QB.chat.send(opponentId, msg)
-      renderMessage('send', message)
-      $('.js-message').val('');
+    QB.chat.send(currentOpponentId, msg)
+    renderMessage('send', message)
+    let dialogId = verifyIfDialogExist(currentOpponentId)
+    console.log('verify if exist dialog id', dialogId)
+    if(dialogId) {
+        removeDialogFromList(dialogId)
+    }else {
+        getDialogs()
+    }
+    scrollTobottom(true)
+    $('.js-message').val('');
+}
+
+let verifyIfDialogExist = function (opponentId){
+    for (let i = 0; i < diaglogsList.length; i++) {
+        dialog = diaglogsList[i];
+        if(dialog.occupants_ids.indexOf(opponentId) != -1) {
+            return dialog._id
+        }
+    }
+    return false;
 }
 
 let renderMessage = function (type, message){
@@ -263,18 +327,67 @@ let renderMessage = function (type, message){
         </div>
     </div>`;
     template =  template.replace('{{message}}', message);
-    $('.messages-container').append(template)
+    $('.messages-container .messages-list').append(template)
 }
 
-let listtenMessages = function (userId, msg){
+let listenMessages = function (userId, msg) {
+    let entryDialogId = msg.extension.dialog_id
+    let dialogInMemory = diaglogsList.filter( x => x._id == entryDialogId)
+    let dialog = false;
+    if(dialogInMemory.length > 0){
+        dialog = dialogInMemory[0]
+        handleIncomingMessages(dialog, msg)
+    } else {
+        getDialogs().then(function (){
+            let dialogInMemory = diaglogsList.filter( x => x._id == entryDialogId)
+            let dialog = false;
+            if(dialogInMemory.length > 0){
+                dialog = dialogInMemory[0]
+                handleIncomingMessages(dialog, msg)
+            }
+        })
+    }
     console.log('message received ', msg)
-    renderMessage('received' ,msg)
 }
 
-let renderUsersList = function (users, ignoreId){
+let handleIncomingMessages = function (dialog, msg){
+    console.log('dialog xd', dialog)
+    if(dialog.occupants_ids.indexOf(currentOpponentId) != -1) {
+        renderMessage('received' , msg.body)
+        scrollTobottom(true)
+        // remove to be able loaded when the user goes back, this way we avoid updating 
+        // the chat history in memory for incoming messages
+        removeDialogFromList(dialog._id)
+    } else {
+        let opponentId = dialog.occupants_ids[1];
+        updateUserBadge(opponentId) 
+    }
+}
+
+let removeDialogFromList = function (dialogId){
+    diaglogHistory[dialogId] = false;
+    console.log('remove index', diaglogsList)
+}
+
+let updateUserBadge = function(userId, reset = false) {
+    let user = $(`.user-list [data-id='${userId}'] .badge`);
+    console.log('update badge ', user, userId)
+    if(reset) {
+        user.html(0)
+        user.addClass('d-none')
+    }else {
+        user.removeClass('d-none')
+        user.html(parseInt(user.html()) + 1)
+    }
+}
+
+let renderUsersList = function (users, currentUserId){
     let htmlTemplate = `<li class="js-user" data-id="{{userId}}">
                             {{name}}
-                            <span class="status"></span>
+                            <div class="d-flex align-items-center">
+                                <span class="badge badge-primary {{show}}">{{countMessages}}</span>
+                                <span class="ml-2 status"></span>
+                            </div>
                         </li>`;
     if(users.items.length > 0) {
         $('.user-list ul').html('');
@@ -282,13 +395,32 @@ let renderUsersList = function (users, ignoreId){
     }
 
     users.items.forEach(element => {
-        if(element.user.id != ignoreId) {
+        let userId = element.user.id;
+        if(userId != currentUserId) {
             html = htmlTemplate
             html = html.replace('{{name}}', element.user.login);
-            html = html.replace('{{userId}}', element.user.id);
+            html = html.replace('{{userId}}', userId);
+            let countUnReadMessages = lookForUnreadMessages(userId);
+            if(countUnReadMessages > 0) {
+                html = html.replace('{{show}}', '');
+                html = html.replace('{{countMessages}}', countUnReadMessages);
+            }else {
+                html = html.replace('{{show}}', 'd-none');
+                html = html.replace('{{countMessages}}', 0);
+            }
             $('.user-list ul').append(html)
         }
     });
+}
+
+let lookForUnreadMessages = function (userId) {
+    let result = 0
+    for (let i = 0; i < diaglogsList.length; i++) {
+        if(diaglogsList[i].occupants_ids.indexOf(userId) != -1) {
+            return diaglogsList[i].unread_messages_count
+        }
+    }
+    return result;
 }
 
 let listenForUserStatus = function (){
